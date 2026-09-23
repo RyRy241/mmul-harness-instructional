5c383d3b83808bd03cf4fa3890ebf7ce9e928543
KoB <142365031+RyRy241@users.noreply.github.com>
2026-09-23 09:54:56 -0700

    Parts 1 and 2

--- dgemm-basic.cpp
+++ dgemm-basic.cpp
@@ -8,5 +8,12 @@ const char* dgemm_desc = "Basic implementation, three-loop dgemm.";
  */
 void square_dgemm(int n, double* A, double* B, double* C) 
 {
-   // insert your code here: implementation of basic matrix multiple
+   for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+         double sum = 0.0;
+         for (int k = 0; k < n; k++)
+            sum += A[i*n + k] * B[k*n + j];
+         C[i*n + j] += sum;
+      }
+   }
 }
--- dgemm-blocked.cpp
+++ dgemm-blocked.cpp
@@ -1,3 +1,5 @@
+#include <cstring>
+#include <vector>
 const char* dgemm_desc = "Blocked dgemm.";
 
 /* This routine performs a dgemm operation
@@ -6,5 +8,47 @@ const char* dgemm_desc = "Blocked dgemm.";
  * On exit, A and B maintain their input values. */
 void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C) 
 {
-   // insert your code here
+   const int bs = block_size;
+
+   std::vector<double> Ablk(bs*bs), Bblk(bs*bs), Cblk(bs*bs);
+
+   for (int ii = 0; ii < n; ii += bs) {
+      for (int jj = 0; jj < n; jj += bs) {
+
+         for (int i = 0; i < bs; ++i) {
+            for (int j = 0; j < bs; ++j) {
+               Cblk[i*bs + j] = C[(ii+i)*n + (jj+j)];
+            }
+         }
+
+         for (int kk = 0; kk < n; kk += bs) {
+
+            for (int i = 0; i < bs; ++i) {
+               for (int k = 0; k < bs; ++k) {
+                  Ablk[i*bs + k] = A[(ii+i)*n + (kk+k)];
+               }
+            }
+
+            for (int i = 0; i < bs; ++i) {
+               for (int j = 0; j < bs; ++j) {
+                  Bblk[i*bs + j] = B[(kk+i)*n + (jj+j)];
+               }
+            }
+
+            for (int i = 0; i < bs; ++i) {
+               for (int k = 0; k < bs; ++k) {
+                  for (int j = 0; j < bs; ++j) {
+                     Cblk[i*bs + j] += Ablk[i*bs + k] * Bblk[k*bs + j];
+                  }
+               }
+            }
+         }
+
+         for (int i = 0; i < bs; ++i) {
+            for (int j = 0; j < bs; ++j) {
+               C[(ii+i)*n + (jj+j)] = Cblk[i*bs + j];
+            }
+         }
+      }
+   }
 }
